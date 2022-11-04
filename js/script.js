6b94df30a777a060fd063e4c580ea55fbaf0c6ef
lubov-nefed <love.nefed@gmail.com>
2022-11-04 16:52:55 +0300

    feat: add cart buttons

--- js/script.js
+++ js/script.js
@@ -1,119 +1,98 @@
 const products = document.getElementById('products');
 const cart = document.getElementById('cart');
+const emptyCartMessage = document.querySelector('#emptyMessage');
+
+document.addEventListener("DOMContentLoaded", getProductsData);
+//?-- Подходит ли это чтобы сделать вызов этой функции при загрузке страницы? Или использовать document.onload?
 
 async function getProductsData() {//Получаем массив данных товаров
     let response = await fetch('https://my-json-server.typicode.com/lubov-nefed/ajax-training/db');
     let content = await response.json();
     let productsJson = content.products;
-    for(obj of productsJson) {
-        let {name, url, ingredients, description, price, oneSlice} = obj;        
-        createProduct(name, url, ingredients, description, price, oneSlice);
+    renderCatalogue(productsJson);
+}
+
+function renderCatalogue(dataArr) {//Создаем разметку для каждого товара в каталоге
+    for (obj of dataArr) {
+        let { name, url, ingredients, description, price, oneSlice } = obj;        
+        products.innerHTML +=`
+        <div class="product">
+        <img class="product__img" src="${url}">
+        <div class="product__text">
+        <h4 class="product__title">${name}</h4>
+        <p class="product__ingredients">${ingredients}</p>
+        <p class="product__description">${description}</p>
+        <p class="product__price">${price}$</p></div>
+        <button class="product-btn btn">Add to Cart</button>
+        </div>
+        `;
+        addListenerToBtn();
+    }    
+}
+
+function addListenerToBtn() {//Добавляем обработчик события на кнопки "Добавить в корзину"
+    const btnCollection = document.querySelectorAll('.btn');
+    btnCollection.forEach(element => {
+        if(element.classList.contains('product-btn')) {
+            element.addEventListener('click', addToCart);          
+        } else if (element.classList.contains('increase-btn')) {            
+            element.addEventListener('click', increaseCartProductQuantity);
+        } else if (element.classList.contains('decrease-btn')) {
+            element.addEventListener('click', decreaseCartProductQuantity);
+        }
     }
-}
-getProductsData();//?-- Как сделать вызов этой функции при загрузке страницы document.onload?
-
-function checkCart() {//?--Как проверить есть ли в корзине сообщение от ом что она пуста и тогла добавить товар + проверить есть ли уже этот товар в корзине илил нет
-    const emptyMessage = document.getElementById('emptyMessage');
-    if (cart.firstElementChild == emptyMessage) emptyMessage.remove();
+    )
 }
 
-function createProduct(name, url, ingredients, description, price, oneSlice) {//Генерируем разметку/контент раздела "товары" в нашем магазине
-    const product = document.createElement('div');//Создаем обертку дл одного товара
-    product.className = 'product';
+function addToCart(event) {               
+    let currentProduct = event.currentTarget.closest('.product');     
+    let currentProductInfo = {
+        title: currentProduct.querySelector('.product__title').innerHTML, 
+        imgSrc: currentProduct.querySelector('.product__img').src, 
+        price: currentProduct.querySelector('.product__price').innerHTML,
+    };
 
-    const productImg = document.createElement('img');
-    productImg.className = 'product__img';
-    productImg.src = url;
-    product.appendChild(productImg);
+    let currentlyAddedProduct = document.getElementById(`${currentProductInfo.title}`);    
 
-    const productText = document.createElement('div');//Создаем обуртку для текстового содержимого товара(название, цена и тп)
-    productText.className = 'product__text';
-    product.appendChild(productText);
-
-    const productName = document.createElement('h4');
-    productName.innerHTML = name;
-    productText.appendChild(productName);
-
-    const productIngredients = document.createElement('p');
-    productIngredients.innerHTML = 'ingredients: ' + ingredients;
-    productText.appendChild(productIngredients);
-
-    const productDescription = document.createElement('p');
-    productDescription.innerHTML = description;
-    productText.appendChild(productDescription);
-
-    const productPrice = document.createElement('p');
-    productPrice.innerHTML = price + '$';
-    productText.appendChild(productPrice);
-
-    const addToCartBtn = document.createElement('button');//Создаем кнопку добавления товара в корзину
-    addToCartBtn.className = 'product-btn';
-    addToCartBtn.innerHTML = 'Add to Cart';
-    addToCartBtn.addEventListener('click', addtoCart)//добавляем событие и функцию добавления в корзину
-    product.appendChild(addToCartBtn);
-
-    products.appendChild(product);
-}
-
-
-function addtoCart(event) {//?--Можно ли как-то через параметр передать в функцию id кннопки/то есть можно ли чем-то заменить currentTarget
-    checkCart();
-    const productName = event.currentTarget.parentElement.firstChild.nextSibling.firstChild.innerHTML;//заменить одинаковые обращения к элементам
-    //На данный момент корзина пуста, элементов нет, узлы есть
-    //?--Почему здесь есть пустые текстовые узлы когда я использую childNodes
-
-    
-    if (cart.firstElementChild && document.getElementById(productName)) {//Если в корзине уже есть хотя бы один элемент. Долго не могла понять что нужно делать без цикла.
-        document.getElementById(productName).querySelector('.cart__input').value++;
-    } else {
-    /*Добавление разметки нового товара в корзину*/
-     const cartProduct = document.createElement('div');
-     cartProduct.className = 'cart-product';
-     cart.appendChild(cartProduct);
-
-     const img = event.currentTarget.parentElement.firstChild.cloneNode(true);
-     cartProduct.appendChild(img);
-
-     const title = event.currentTarget.parentElement.firstChild.nextSibling.firstChild.cloneNode(true);
-     cartProduct.appendChild(title);
-
-     cartProduct.id = title.textContent;
-
-     const cartQuantity = document.createElement('div');
-     cartQuantity.className = 'cart-quantity';
-     cartProduct.appendChild(cartQuantity);
-
-     const minusBtn = document.createElement('button');
-     minusBtn.innerHTML = '-';
-     minusBtn.className = 'cart-btn';
-     minusBtn.setAttribute('title', 'Decrease');
-     cartQuantity.appendChild(minusBtn);
-
-     const cartInput = document.createElement('input');
-     cartInput.className = 'cart__input';
-     cartInput.value += 1;
-     cartInput.setAttribute('maxlength', '3');
-     cartQuantity.appendChild(cartInput);
-
-     const plusBtn = document.createElement('button');
-     plusBtn.innerHTML = '+';
-     plusBtn.className = 'cart-btn';
-     plusBtn.setAttribute('title', 'Increase');
-     cartQuantity.appendChild(plusBtn);
-
-     const cartDelete = document.createElement('button');
-     cartDelete.className = 'cart-delete';
-     cartDelete.setAttribute('title', 'Delete');
-     const deleteImg = document.createElement('img');
-     deleteImg.className = 'cart-delete__img';
-     deleteImg.src = 'https://raw.githubusercontent.com/lubov-nefed/ajax-training/63683b87da2467a40237323e4fcd01f93fcbde46/images/delete-icon.svg';
-     cartDelete.appendChild(deleteImg);
-     cartProduct.appendChild(cartDelete);      
+    if (currentlyAddedProduct) {
+        increaseCartProductQuantity(event);
+    } else {//с инпутом не получилось сделать он обнулялся при каждом добавлении товара в корзину
+        /*Добавление разметки нового товара в корзину*/
+        cart.innerHTML += `
+        <div class="cart-product" id="${currentProductInfo.title}">
+            <img class="product__img" src="${currentProductInfo.imgSrc}"/>
+            <h4 class="product__title">${currentProductInfo.title}</h4>
+            <div class="cart-quantity">
+                <button class="cart-btn decrease-btn btn" title="Decrease">-</button>
+                <span class="cart__quantity">1</span> 
+                <button class="cart-btn increase-btn btn" title="Increase">+</button>                
+            </div>
+            <button class="cart-delete btn" title="Delete">
+                <img class="cart-delete__img" 
+                src="https://raw.githubusercontent.com/lubov-nefed/ajax-training/63683b87da2467a40237323e4fcd01f93fcbde46/images/delete-icon.svg"
+                />
+            </button>
+            <p class="cart-product__sum">
+                ${currentProductInfo.price}
+            </p>
+        </div>
+        `
+        addListenerToBtn();
     }
- 
+    toggleEmptyCartMessage(); 
+}
 
-//Добавляет цену одного товара из разметки
-/*const price = event.currentTarget.parentElement.firstChild.nextSibling.lastChild.cloneNode(true);
-price.innerHTML = price.innerHTML.slice(0, -1);
-cartProduct.appendChild(price);*/
+function toggleEmptyCartMessage() {
+    cart.innerHTML  !== '' ? emptyCartMessage.classList.add('hidden') : emptyCartMessage.classList.remove('hidden');
+}
+
+function increaseCartProductQuantity(event) {
+    let productTitle = event.currentTarget.parentElement.querySelector('.product__title') || event.currentTarget.parentElement.parentElement.querySelector('.product__title');
+    +document.getElementById(productTitle.innerHTML).querySelector('.cart__quantity').innerHTML++;    
+}
+
+function decreaseCartProductQuantity(event) {
+    let productTitle = event.currentTarget.parentElement.parentElement.querySelector('.product__title');
+    let productQuantity = document.getElementById(productTitle.innerHTML).querySelector('.cart__quantity').innerHTML;
+    if (productQuantity > 1) document.getElementById(productTitle.innerHTML).querySelector('.cart__quantity').innerHTML--;//?--Почему не работает productQuantity--;  
 }
