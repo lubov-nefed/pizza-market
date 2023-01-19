a9e53bebb0d09a470ba80e23de5a52bcf6317fcd
Sasha Sushko <sasha@sushko.me>
2023-01-19 10:26:16 +0200

    fix: separate modules

--- js/load-data.js
+++ js/load-data.js
@@ -1,12 +1,16 @@
-import {renderCatalogue} from './render.js';
-export async function getProductsData() {//Получаем массив данных товаров
+// Получаем массив данных товаров
+export async function getProductsData() {
     let content = [];
     try {
         let response = await fetch('https://my-json-server.typicode.com/lubov-nefed/ajax-training/db');
-        if (response.ok) content = await response.json();
+        if (response.ok) {
+            content = await response.json();
+            return content
+        } else {
+            throw Error('Response is not ok');
+        }
     } catch (err) {
-        console.dir(err);
+        console.error(err);
+        return content;
     }
-    
-    renderCatalogue(content.products);
-} 
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 import {getProductsData} from './load-data.js';
-getProductsData();
+import {renderCatalogue} from './render.js';
+
+const data = await getProductsData();
+
+renderCatalogue(data.products);
