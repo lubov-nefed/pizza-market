3df2d745e0ef390fad6113b2651cbd32e214ab58
lubov-nefed <love.nefed@gmail.com>
2023-02-02 16:36:24 +0300

    refactor: delete uninformative comments

--- js/load-data.js
+++ js/load-data.js
@@ -6,12 +6,9 @@ async function getProductsData(onsuccess, onerror) {
     const response = await fetch('https://my-json-server.typicode.com/lubov-nefed/ajax-training/db');
     if (response.ok) {
       content = await response.json();
-      //return content.products;
       onsuccess(content.products);
     } else throw Error('Response is not ok');
   } catch (err) {
-    // eslint-disable-next-line no-console
-    //console.error(err);
     onerror();
   }
   return content;
--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import { getProductsData } from './load-data.js';
 import { createCatalogue } from './catalogue.js';
 
-//const data = await getProductsData();
 getProductsData(createCatalogue, ()=>{renderErr()});
-
-createCatalogue(data);
