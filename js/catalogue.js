911c626085a56aea312b3978c906fd701467d6a4
lubov-nefed <love.nefed@gmail.com>
2024-04-04 15:02:14 +0300

    refactor: fix linter suggested errors

--- js/catalogue.js
+++ js/catalogue.js
@@ -1,13 +1,12 @@
-import { addToCart } from './cart.js';
-import { renderMarkup } from './render.js';
-
-export { createCatalogue };
+import addToCart from './cart.js';
+import renderMarkup from './render.js';
 
 function createCatalogue(dataArr) {
   const products = document.querySelector('.products');
   if (dataArr.length) {
-    dataArr.forEach((obj) => { renderMarkup(obj, products) });
+    dataArr.forEach((obj) => { renderMarkup(obj, products); });
   }
   const btnCollection = document.querySelectorAll('.product-btn');
   btnCollection.forEach((element) => element.addEventListener('click', addToCart));
 }
+export default createCatalogue;
--- js/load-data.js
+++ js/load-data.js
@@ -1,14 +1,16 @@
-export { getProductsData };
-
 async function getProductsData(onsuccess, onerror) {
   let content = [];
   try {
-    const response = await fetch('https://my-json-server.typicode.com/lubov-nefed/ajax-training/db');
+    const response = await fetch(
+      "https://my-json-server.typicode.com/lubov-nefed/ajax-training/pizza-market"
+    );
     if (response.ok) {
       content = await response.json();
       onsuccess(content.products);
-    } else throw Error('Response is not ok');
+    } else throw Error("Response is not ok");
   } catch (err) {
     onerror();
   }
 }
+
+export default getProductsData;
